c77e4780278a7ab4f09fd7e713b9c2a76f131911
Kalana <kalanasasanka24@gmail.com>
2025-04-29 15:44:37 +1000

    mod: add msg_queue mutex initialization in init_mutex function

--- src/mh/mutex_h.cpp
+++ src/mh/mutex_h.cpp
@@ -4,6 +4,7 @@
 
 SemaphoreHandle_t ntp_client_mh;  // Mutex handle
 SemaphoreHandle_t rf95_mh;  // Mutex handle
+SemaphoreHandle_t msg_queue_mh;  // Mutex handle
 
 void init_mutex(device_state_t state)
 {
@@ -25,10 +26,32 @@ void init_mutex(device_state_t state)
                 DEBUG();
                 PRINT_ERROR("Failed to create mutex for time_client");
             }
+
+            // //init msg queue mutex
+            msg_queue_mh = xSemaphoreCreateMutex();
+            if (msg_queue_mh == NULL)
+            {
+                DEBUG();
+                PRINT_ERROR("Failed to create mutex for msg queue");
+            }else{
+                printf("msg queue mutex created\n");
+            }
             
             break;
         case SENSOR_STATE:
             //TODO: init sensor mutex's
+
+            // //TODO REMOVE///////////
+            // // //init msg queue mutex
+            // msg_queue_mh = xSemaphoreCreateMutex();
+            // if (msg_queue_mh == NULL)
+            // {
+            //     DEBUG();
+            //     PRINT_ERROR("Failed to create mutex for msg queue");
+            // }else{
+            //     printf("msg queue mutex created\n");
+            // }
+            // ///////////////////////
             break;
         
         default:
--- src/mh/mutex_h.h
+++ src/mh/mutex_h.h
@@ -5,5 +5,6 @@
 
 extern SemaphoreHandle_t ntp_client_mh;  // Mutex handle
 extern SemaphoreHandle_t rf95_mh;  // Mutex handle
+extern SemaphoreHandle_t msg_queue_mh;  // Mutex handle
 
 void init_mutex(device_state_t state);
