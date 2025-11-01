02eeb5959ade1c20b4cefd8702669eb21a1e0886
Lucas Aponso <s3896348@student.rmit.edu.au>
2025-11-01 19:50:47 +1100

    add: added compile time var to select deep sleep or normal operation

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include "Communication.hpp"
 #include "esp_log.h"
-
 extern "C" {
-    #include "freertos/FreeRTOS.h"
-    #include "freertos/task.h"
-    #include "nvs_flash.h"
-    #include "esp_sleep.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "nvs_flash.h"
+#include "esp_sleep.h"
 }
-
 #include "esp_log.h"
 #include "esp_event.h"
 #include "esp_wifi.h"
@@ -20,8 +18,7 @@ extern "C" {
 #include "BatteryPacket.hpp"
 #include "Config.hpp"
 
-// constexpr int sleep_time_sec = 6 * 60 * 60;
-constexpr int sleep_time_sec = 30;
+constexpr int sleep_time_sec = 6 * 60 * 60;
 
 void init_nvs()
 {
@@ -31,39 +28,48 @@ void init_nvs()
         ESP_ERROR_CHECK(nvs_flash_erase());
         ret = nvs_flash_init();
     }
-    
     ESP_ERROR_CHECK(ret);
 }
 
 extern "C" void app_main(void) 
 {
-    printf("MY_COMPILE_TIME_VAR = %d\n", MY_COMPILE_TIME_VAR);
-    init_nvs();
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    
-    Communication comm(ConnectionType::WIFI);
-
-    if (comm.connect())
+    while (1)
     {
-        BatteryPacket battery(NODE_ID, URI_BATT);
+        init_nvs();
+        ESP_ERROR_CHECK(esp_netif_init());
+        ESP_ERROR_CHECK(esp_event_loop_create_default());
+        
+        Communication comm(ConnectionType::WIFI);
 
-        if (!battery.readFromBMS()) 
+        if (comm.connect())
         {
-            ESP_LOGE("MAIN", "Failed to read battery from BMS");
-        } 
-        else 
-        {
-            battery.sendPacket();
-        }
+            BatteryPacket battery(NODE_ID, BATT_URI, 0, 0, BATT_TAG);
 
-        if (comm.isConnected()) 
-        {
-            comm.disconnect();
+            if (!battery.readFromBMS())
+            {
+                ESP_LOGE("MAIN", "Failed to read battery from BMS");
+            } 
+            else 
+            {
+                battery.sendPacket();
+            }
+            
+            if (comm.isConnected()) 
+            {
+                comm.disconnect();
+            }
         }
+        
+        #if DEEP_SLEEP_EN == 1
+            break;
+        #else
+            vTaskDelay(pdMS_TO_TICKS(sleep_time_sec * 1000));
+        #endif
     }
-
-    ESP_LOGI("MAIN", "Going into deep sleep for %d seconds...", sleep_time_sec);
-    esp_sleep_enable_timer_wakeup(sleep_time_sec * 1000000ULL);  // Convert to microseconds
-    esp_deep_sleep_start();
+    
+    #if DEEP_SLEEP_EN == 1
+        ESP_LOGI("MAIN", "Going into deep sleep for %d seconds...", sleep_time_sec);
+        esp_sleep_enable_timer_wakeup(sleep_time_sec * 1000000ULL);  // Convert to microseconds
+        esp_deep_sleep_start();
+    #endif
 }
