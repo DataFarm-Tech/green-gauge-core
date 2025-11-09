b6a9b5d9051e51d32c835a11d824ba20d9acceb6
Lucas Aponso <s3896348@student.rmit.edu.au>
2025-11-09 18:45:24 +1100

    feat: added hmac key to activate endpoint.

--- main/include/Config.hpp
+++ main/include/Config.hpp
@@ -5,7 +5,7 @@
 constexpr char NODE_ID[] = "nod123";
 constexpr char BATT_URI[] = "coap://45.79.239.100/battery";
 constexpr char DATA_URI[] = "coap://45.79.239.100/reading";
-constexpr char ACT_URI[] = "coap://45.79.239.100/activate";
+constexpr char ACT_URI[] = "coap://192.168.1.44/activate";
 
 constexpr char BATT_TAG[] = "BatteryPacket";
 constexpr char DATA_TAG[] = "DataPacket";
--- main/src/ActivatePacket.cpp
+++ main/src/ActivatePacket.cpp
@@ -1,18 +1,38 @@
 #include "packet/ActivatePacket.hpp"
+#include <mbedtls/md.h>
+#include "Config.hpp"
+
+
+
+void ActivatePacket::computeKey(uint8_t * out_hmac) const {
+    mbedtls_md_context_t ctx;
+    mbedtls_md_init(&ctx);
+    mbedtls_md_setup(&ctx, mbedtls_md_info_from_type(MBEDTLS_MD_SHA256), 1);
+
+    // Use raw bytes of the secretKey
+    mbedtls_md_hmac_starts(&ctx, secretKey, HMAC_KEY_SIZE);
+
+    // No update, finish immediately
+    mbedtls_md_hmac_finish(&ctx, out_hmac);
+    mbedtls_md_free(&ctx);
+}
 
 const uint8_t * ActivatePacket::toBuffer()
 {
     CborEncoder encoder, mapEncoder;
-    cbor_encoder_init(&encoder, buffer, BUFFER_SIZE, 0);  // use member buffer
+    cbor_encoder_init(&encoder, buffer, BUFFER_SIZE, 0);
 
-    // Root map with 2 elements: node_id, gps
-    if (cbor_encoder_create_map(&encoder, &mapEncoder, 2) != CborNoError) 
+    // Root map with 3 elements: node_id, gps, key
+    if (cbor_encoder_create_map(&encoder, &mapEncoder, 3) != CborNoError) 
     {
         ESP_LOGE(TAG.c_str(), "Failed to create root map");
         return nullptr;
     }
 
-    //TODO: Get GPS Coordinates from GPS MOD
+    uint8_t hmac[HMAC_KEY_SIZE];
+    computeKey(hmac);
+
+    // TODO: Get GPS Coordinates from GPS MOD
     std::string gps_coor = "00.00.00.00";
 
     // node_id
@@ -23,7 +43,11 @@ const uint8_t * ActivatePacket::toBuffer()
     cbor_encode_text_stringz(&mapEncoder, "gps");
     cbor_encode_text_stringz(&mapEncoder, gps_coor.c_str());
 
-    // Close root map
+    // key as raw bytes
+    cbor_encode_text_stringz(&mapEncoder, "key");
+    cbor_encode_byte_string(&mapEncoder, hmac, sizeof(hmac));
+
+    // Close map
     cbor_encoder_close_container(&encoder, &mapEncoder);
 
     bufferLength = cbor_encoder_get_buffer_size(&encoder, buffer);
