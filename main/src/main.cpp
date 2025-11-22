35d834f061b59140fafc973a2eacc4060bd0694c
Lucas Aponso <s3896348@student.rmit.edu.au>
2025-11-22 02:14:02 +0000

    mod: cleaned-up main file

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -27,60 +27,69 @@ extern "C" {
 #include "Node.hpp"
 #include "UARTConsole.hpp"
 #include "CLI.hpp"
+#include "ota/OTAUpdater.hpp"
 
 Node nodeId;
 DeviceConfig g_device_config = { false, nodeId };
 
 
 extern "C" void app_main(void)
-{
-	vTaskDelay(pdMS_TO_TICKS(2000));
-    printf("ENDING KERNEL STARTUP...\n");
+{   
+    // Check wakeup cause and reset reason
+    esp_sleep_wakeup_cause_t wakeup_reason;
+    esp_reset_reason_t reset_reason;
+    OTAUpdater ota;
+    EEPROMConfig eeprom;
+    Communication comm(ConnectionType::WIFI);
+    ActivatePacket activate(g_device_config.nodeId.getNodeID(), ACT_URI, ACT_TAG);
+    ReadingPacket readings(g_device_config.nodeId.getNodeID(), DATA_URI, DATA_TAG);
+    const char* url = "http://45.79.118.187:8080/release/latest/cn1.bin";
+
+
     // Start CLI task
     UARTConsole::init(115200);
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     CLI::start();
 
-    // Original application code (commented out for CLI-only mode)
+    wakeup_reason = esp_sleep_get_wakeup_cause();
+    reset_reason = esp_reset_reason();
     
+    // Original application code
     vTaskDelay(pdMS_TO_TICKS(500));
     
-    EEPROMConfig eeprom;
-    if (!eeprom.begin()) {
+    if (!eeprom.begin()) 
+    {
         ESP_LOGE("MAIN", "Failed to open EEPROM config");
         return;
     }
 
-    if (!eeprom.loadConfig(g_device_config)) {
+    if (!eeprom.loadConfig(g_device_config)) 
+    {
         ESP_LOGW("MAIN", "No previous config found, initializing default...");
         g_device_config.has_activated = false;
         eeprom.saveConfig(g_device_config);
     }
 
-    while (1) {
-
-        Communication comm(ConnectionType::WIFI);
-
-        if (comm.connect()) {
-            if (!g_device_config.has_activated) {
+    while (1) 
+    {
+        if (comm.connect()) 
+        {
+            if (!g_device_config.has_activated) 
+            {
                 ESP_LOGI("MAIN", "Sending activation packet...");
-                ActivatePacket activate(g_device_config.nodeId.getNodeID(), ACT_URI, ACT_TAG);
                 activate.sendPacket();
                 g_device_config.has_activated = true;
                 eeprom.saveConfig(g_device_config);
-            } else {
+            } 
+            else 
+            {
                 ESP_LOGI("MAIN", "Already activated.");
             }
 
-            ReadingPacket readings(g_device_config.nodeId.getNodeID(), DATA_URI, DATA_TAG);
             ESP_LOGI("MAIN", "Collecting sensor readings...");
             readings.readSensor();
             readings.sendPacket();
-            
-            for (;;) {
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
             
             // BatteryPacket battery(g_device_config.nodeId.getNodeID(), BATT_URI, 0, 0, BATT_TAG);
             // if (!battery.readFromBMS()) {
@@ -89,23 +98,46 @@ extern "C" void app_main(void)
             //     ESP_LOGI("MAIN", "Sending battery packet...");
             //     battery.sendPacket();
             // }
+
+            // Only run OTA update on power-on (not on reboot or deep sleep wakeup)
+            if (reset_reason == ESP_RST_POWERON) 
+            {
+                printf("Power-on detected. Checking for OTA update...\n");
+                   
+                if (ota.update("http://45.79.118.187:8080/release/latest/cn1.bin")) 
+                {
+                    printf("OTA OK. Rebooting...\n");
+                    vTaskDelay(pdMS_TO_TICKS(2000));
+                    esp_restart();
+                } 
+                else 
+                {
+                    printf("OTA FAILED or no update needed\n");
+                }
+            }
+
+            for (;;) //WILL REMOVE THIS LATER...
+            {
+                vTaskDelay(pdMS_TO_TICKS(1000));
+            }
+
             
-            // if (comm.isConnected())
-            //     comm.disconnect();
+            if (comm.isConnected())
+                comm.disconnect();
         }
 
         eeprom.close();
 
-#if DEEP_SLEEP_EN == 1
-        break;
-#else
-        vTaskDelay(pdMS_TO_TICKS(sleep_time_sec * 1000));
-#endif
+        #if DEEP_SLEEP_EN == 1
+            break;
+        #else
+            vTaskDelay(pdMS_TO_TICKS(sleep_time_sec * 1000));
+        #endif
     }
 
-#if DEEP_SLEEP_EN == 1
-    ESP_LOGI("MAIN", "Going into deep sleep...");
-    esp_sleep_enable_timer_wakeup(sleep_time_sec * 1000000ULL);
-    esp_deep_sleep_start();
-#endif
+    #if DEEP_SLEEP_EN == 1
+        ESP_LOGI("MAIN", "Going into deep sleep...");
+        esp_sleep_enable_timer_wakeup(sleep_time_sec * 1000000ULL);
+        esp_deep_sleep_start();
+    #endif
 }
